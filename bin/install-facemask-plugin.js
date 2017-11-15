6c41c3ade33fe307521cc907f14d1421dbc4b951
Andy Creeth <acreeth@gmail.com>
2017-11-15 12:24:43 -0800

    more progress still not working

--- bin/install-facemask-plugin.js
+++ bin/install-facemask-plugin.js
@@ -10,42 +10,34 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const childProcess = require('child_process');
-const rimraf = require('rimraf');
 
-const archivePath = path.join(os.tmpdir(), `node-obs-${FACEMASK_VERSION}.zip`);
+const archivePath = path.join(os.tmpdir(), `obs-facemask-${FACEMASK_VERSION}.zip`);
 const archive = fs.createWriteStream(archivePath);
 
 const zipExe = path.resolve(__dirname, '..', 'node_modules', '7zip-bin-win', 'x64', '7za.exe');
 const slobsDir = path.resolve(__dirname, '..');
+const nodeObsPath = path.join(slobsDir, 'node-obs');
 
-const continuePromise = new Promise(resolve => {
-  const nodeObsPath = path.join(slobsDir, 'node-obs');
+if (!fs.existsSync(nodeObsPath)) {
+  throw new Error('Node OBS must be installed! Try running "yarn install-node-obs" first.');
+}
 
-  if (fs.existsSync(nodeObsPath)) {
-    rimraf(nodeObsPath, resolve);
-  } else {
-    resolve();
-  }
+const releaseUrl = `https://github.com/stream-labs/obs-facemask-plugin/releases/download/` +
+  `${FACEMASK_VERSION}/obs-facemask-plugin-${FACEMASK_VERSION}.zip`;
+
+archive.on('finish', () => {
+  childProcess.exec(`"${zipExe}" x "${archivePath}" -o"${slobsDir}"`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Extraction error: ${error}`);
+      return;
+    }
+
+    console.log(stdout);
+    console.log(stderr);
+  });
 });
 
-continuePromise.then(() => {
-  const releaseUrl = `https://github.com/stream-labs/node-obs/releases/download/` +
-    `${FACEMASK_VERSION}/obs-facemask-plugin-${FACEMASK_VERSION}.zip`;
-
-  archive.on('finish', () => {
-    childProcess.exec(`"${zipExe}" x "${archivePath}" -o"${slobsDir}"`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Extraction error: ${error}`);
-          return;
-        }
-
-        console.log(stdout);
-        console.log(stderr);
-    });
-  });
-
-  https.get(releaseUrl, response => {
-    // Follow redirect
-    https.get(response.headers.location, response => response.pipe(archive));
-  });
+https.get(releaseUrl, response => {
+  // Follow redirect
+  https.get(response.headers.location, response => response.pipe(archive));
 });
